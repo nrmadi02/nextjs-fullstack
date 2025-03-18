8f0a4ed035a2cef2f5580d6362f5e3496e614ef3
nrmadi02 <ahmadinur2202@gmail.com>
2025-03-18 23:09:08 +0800

    feat: setup react-query

---
+++ src/common/components/layouts/query-client.layout.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function QueryClientLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
